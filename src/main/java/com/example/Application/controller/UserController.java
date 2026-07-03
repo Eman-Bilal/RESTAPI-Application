b0383ced842ea2f3e3d102040a1f4b4205b754bd
fa22-bcs-088-svg <imaan2726@gmail.com>
2026-07-03 17:27:21 +0500

    Controller added

--- src/main/java/com/example/Application/controller/UserController.java
+++ src/main/java/com/example/Application/controller/UserController.java
@@ -1,5 +1,40 @@
 package com.example.Application.controller;
 
+import org.springframework.web.bind.annotation.*;
+import java.util.HashMap;
+import java.util.List;
+
+@RestController
+@RequestMapping("/user/")
 public class UserController {
+
+    List<HashMap<String, String>> list;
+
+    public UserController(List<HashMap<String, String>> list) {
+        this.list = list;
+    }
+
+    @GetMapping("getAll")
+    public List<HashMap<String, String>> getAll(){
+        return list ;
+    }
+
+    @PostMapping("create")
+    public List<HashMap<String, String>> create(@RequestBody HashMap<String,String> userRecord){
+        list.add(userRecord);
+        return list;
+    }
+
+    @PutMapping("update/{id}")
+    public String update(){
+
+        return "Student Record Updated";
+    }
+
+    @DeleteMapping("delete/{id}")
+    public String delete(){
+
+        return "Student deleted";
+    }
 
 }
