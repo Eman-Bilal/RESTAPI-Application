6c2e61fe51caf4d9ea92c183f8f78d66577592d4
Eman Bilal <fa22-bcs-088@cuilahore.edu.pk>
2026-07-04 18:36:37 +0500

    All controllers added

--- src/main/java/com/example/Application/controller/UserController.java
+++ src/main/java/com/example/Application/controller/UserController.java
@@ -4,8 +4,22 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.List;
 
+//[
+//        {
+//        "name": "Ahmed Ali",
+//        "cnic": "42101-1234567-1",
+//        "email": "ahmed.ali@example.com",
+//        "phoneNo": "+923001234567"
+//        },
+//        {
+//        "name": "Ayesha Khan",
+//        "cnic": "35202-9876543-2",
+//        "email": "ayesha.khan@example.com",
+//        "phoneNo": "+923219876543"
+//        }
+//        ]
 @RestController
-@RequestMapping("/user/")
+@RequestMapping("/")
 public class UserController {
 
     List<HashMap<String, String>> list;
@@ -26,14 +40,14 @@ public class UserController {
     }
 
     @PutMapping("update/{id}")
-    public String update(){
-
-        return "Student Record Updated";
+    public String update(@PathVariable int id, @RequestBody HashMap<String,String> userRecord ){
+        list.set(id, userRecord);
+        return "Student Record Updated: "+ userRecord;
     }
 
     @DeleteMapping("delete/{id}")
-    public String delete(){
-
+    public String delete(@PathVariable int id) {
+        list.remove(id);
         return "Student deleted";
     }
 
